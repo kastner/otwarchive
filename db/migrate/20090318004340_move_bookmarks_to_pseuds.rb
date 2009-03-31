a7f42b40973b2203db9848205519f93e1246b133
ambtus <ambtus@ec6d4a5c-9048-0410-a62c-f7db8096d385>
2009-03-31 18:27:09 +0000

    messed up the merge. fixing deploy branch

--- db/migrate/20090318004340_move_bookmarks_to_pseuds.rb
+++ db/migrate/20090318004340_move_bookmarks_to_pseuds.rb
@@ -1,24 +1,15 @@
 class MoveBookmarksToPseuds < ActiveRecord::Migration
   def self.up
     add_column :bookmarks, :pseud_id, :integer, :null => false
-<<<<<<< .working
-    #remove_column :bookmarks, :user_id
-=======
     # remove later after bookmarks have been migrated
     # but make null okay
     #remove_column :bookmarks, :user_id
     change_column :bookmarks, :user_id, :integer, :null => true
->>>>>>> .merge-right.r1193
   end
 
   def self.down
-<<<<<<< .working
-    #add_column :bookmarks, :user_id, :integer, :null => false
-    Bookmark.all.each do |bookmark|
-=======
     add_column :bookmarks, :user_id, :integer, :null => false
     Bookmark.all.each do |bookmark|
->>>>>>> .merge-right.r1193
       bookmark.update_attribute(:user_id, bookmark.pseud.user.id)
     end
     remove_column :bookmarks, :pseud_id
