8b14db1de6449e3af18399672ba1eeeda77dc67b
ambtus <ambtus@ec6d4a5c-9048-0410-a62c-f7db8096d385>
2009-04-03 05:09:49 +0000

    bringing deploy branch in sync with development branch. no omissions

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -8,12 +8,10 @@ class InboxController < ApplicationController
   end
   
   def show 
-    @unread = @user.inbox_comments.count(:conditions => {:read => false})   
-    order = 'created_at ' + (params[:sort_by_date] || 'DESC')     
-    read = (params[:filter_read].blank? || params[:filter_read] == 'all') ? [true, false] : params[:filter_read] == 'true'
-    replied_to = (params[:filter_replied_to].blank? || params[:filter_replied_to] == 'all') ? [true, false] : params[:filter_replied_to] == 'true'
-    @inbox_comments = @user.inbox_comments.all(:order => order, :conditions => {:read => read, :replied_to => replied_to}, :include => [:feedback_comment => :pseud])
-    @select_read, @select_replied_to, @select_date = params[:filter_read], params[:filter_replied_to], params[:sort_by_date]
+    @unread = @user.inbox_comments.count(:conditions => {:read => false})
+    filters = params[:filters] || {}   
+    @inbox_comments = @user.inbox_comments.find_by_filters(filters)
+    @select_read, @select_replied_to, @select_date = filters[:read], filters[:replied_to], filters[:date]
   end
   
   def reply
@@ -22,17 +20,15 @@ class InboxController < ApplicationController
   end
 
   def update
-    @selected_inbox_comment_ids = params[:inbox_comments].keys if params[:inbox_comments]
-    if @selected_inbox_comment_ids.blank?
+    begin
+      @inbox_comments = InboxComment.find(params[:inbox_comments].keys)
+      case params[:commit]
+        when 'read' then @inbox_comments.each { |i| i.update_attribute(:read, true) }
+        when 'unread' then @inbox_comments.each { |i| i.update_attribute(:read, false) }
+        when 'delete from inbox' then @inbox_comments.each { |i| i.destroy }
+      end    
+    rescue
       flash[:warning] = t('please_select', :default => "Please select something first")
-    else
-      if params[:commit] == "read"
-        @selected_inbox_comment_ids.each {|inbox_comment| InboxComment.find(inbox_comment).update_attribute(:read, true) }
-      elsif params[:commit] == "unread"
-        @selected_inbox_comment_ids.each {|inbox_comment| InboxComment.find(inbox_comment).update_attribute(:read, false) }
-      elsif params[:commit] == "delete from inbox"
-        @selected_inbox_comment_ids.each {|inbox_comment| InboxComment.find(inbox_comment).destroy }
-      end
     end
     redirect_to user_inbox_path(@user)
   end
--- app/models/inbox_comment.rb
+++ app/models/inbox_comment.rb
@@ -1,4 +1,24 @@
 class InboxComment < ActiveRecord::Base
+  validates_presence_of :user_id
+  validates_presence_of :feedback_comment_id
+	
   belongs_to :user
   belongs_to :feedback_comment, :class_name => 'Comment'
+  
+  # Filters inbox comments by read and/or replied to and sorts by date
+  named_scope :find_by_filters, lambda { |filters| 
+    read = case filters[:read]
+      when 'true' then true
+      when 'false' then false
+      else [true, false]
+    end
+    replied_to = case filters[:replied_to]
+      when 'true' then true
+      when 'false' then false
+      else [true, false]
+    end
+    { :order => 'created_at ' + (filters[:date] || 'DESC'),
+      :conditions => {:read => read, :replied_to => replied_to},
+      :include => [:feedback_comment => :pseud] }
+  }
 end
